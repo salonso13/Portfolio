52eea438f086de5590f4dec93169f125a1bae402
Sergio Alonso Revueltas <58438643+salonso13@users.noreply.github.com>
2025-01-17 22:29:28 +0100

    Update cover-particles.tsx

--- components/cover-particles.tsx
+++ components/cover-particles.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 
 const CoverParticles = () => {
-  const [setInit] = useState(false);
-
   useEffect(() => {
     initParticlesEngine(async (engine) => {
       await loadSlim(engine);
